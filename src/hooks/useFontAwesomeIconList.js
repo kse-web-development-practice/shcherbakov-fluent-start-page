b12487fe60282925ff37cc010da965239409de6b
Andrii Shcherbakov <andrey.shcherbakov05@gmail.com>
2024-06-30 02:00:47 +0300

    Fix Font Awesome icon not available when loading icon favicon first instead of icon picker

--- src/hooks/useFontAwesomeIconList.js
+++ src/hooks/useFontAwesomeIconList.js
@@ -1,9 +1,5 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-
-library.add(fas);
-library.add(fab);
 
 const solidIconsWhiteList = [
 	'faHouse',
