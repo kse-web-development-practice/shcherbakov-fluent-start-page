7ef3015f219515aa0526a8dc3ad0cba31c5823b3
Andrii Shcherbakov <andrey.shcherbakov05@gmail.com>
2024-07-17 09:31:13 +0300

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
 	testEnvironment: 'jsdom',
+	collectCoverageFrom: ['src/hooks/**/*.js', 'src/services/**/*.js'],
 	transform: {
 		'\\.[jt]sx?$': 'babel-jest'
 	}
